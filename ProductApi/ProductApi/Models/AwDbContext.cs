191d3fb8f647f9ecdc4b3bb9e66e0bd990f91345
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-10-30 10:52:40 +0300

    db: add context and connection string

---
+++ ProductApi/ProductApi/Models/AwDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Models
+{
+    public class AwDbContext : DbContext
+    {
+        public AwDbContext(): base()
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}

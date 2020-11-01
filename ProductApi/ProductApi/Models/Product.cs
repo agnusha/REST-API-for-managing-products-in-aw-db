7ed6ef6ee5dcdba6cf034e8943a12e189de634dd
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-11-01 13:09:18 +0300

    test: add ProductsControllerTests

--- ProductApi/ProductApi/Models/Product.cs
+++ ProductApi/ProductApi/Models/Product.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ProductApi.Models
 {
+    [ExcludeFromCodeCoverage]
+
     public partial class Product
     {
         public int ProductId { get; set; }
---
+++ ProductApi/XUnitTestProject/ProductsControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

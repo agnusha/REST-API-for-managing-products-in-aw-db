00a0cf341a11346e38d4d860f5efc2fe698ce132
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-11-01 14:54:42 +0300

    test: add unit tests

---
+++ ProductApi/XUnitTestProject/ProductsController.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class ProductsControllerBase
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

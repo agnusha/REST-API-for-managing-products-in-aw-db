9d4b21bdf8bdc24867d74aef66be8d82176b9963
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-11-17 22:59:08 +0300

    feat: add Document entity

--- ProductApi/ProductApi/Models/AdventureWorks2019Context.cs
+++ ProductApi/ProductApi/Models/AdventureWorks2019Context.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace ProductApi.Models
 {
@@ -143,6 +141,79 @@ namespace ProductApi.Models
                     .HasMaxLength(3)
                     .IsFixedLength()
                     .HasComment("Unit of measure for Weight column.");
+            });
+
+
+            modelBuilder.Entity<Document>(entity =>
+            {
+                entity.HasKey(e => e.DocumentNode)
+                    .HasName("PK_Document_DocumentNode");
+
+                entity.ToTable("Document", "Production");
+
+                entity.HasComment("Product maintenance documents.");
+
+                entity.HasIndex(e => new { e.DocumentLevel, e.DocumentNode }, "AK_Document_DocumentLevel_DocumentNode")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Rowguid, "AK_Document_rowguid")
+                    .IsUnique();
+
+                entity.HasIndex(e => new { e.FileName, e.Revision }, "IX_Document_FileName_Revision");
+
+                entity.HasIndex(e => e.Rowguid, "UQ__Document__F73921F7A5124F78")
+                    .IsUnique();
+
+                entity.Property(e => e.DocumentNode).HasComment("Primary key for Document records.");
+
+                entity.Property(e => e.ChangeNumber).HasComment("Engineering change approval number.");
+
+                entity.Property(e => e.Document1)
+                    .HasColumnName("Document")
+                    .HasComment("Complete document.");
+
+                entity.Property(e => e.DocumentLevel)
+                    .HasComputedColumnSql("([DocumentNode].[GetLevel]())", false)
+                    .HasComment("Depth in the document hierarchy.");
+
+                entity.Property(e => e.DocumentSummary).HasComment("Document abstract.");
+
+                entity.Property(e => e.FileExtension)
+                    .IsRequired()
+                    .HasMaxLength(8)
+                    .HasComment("File extension indicating the document type. For example, .doc or .txt.");
+
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasMaxLength(400)
+                    .HasComment("File name of the document");
+
+                entity.Property(e => e.FolderFlag).HasComment("0 = This is a folder, 1 = This is a document.");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("Date and time the record was last updated.");
+
+                entity.Property(e => e.Owner).HasComment("Employee who controls the document.  Foreign key to Employee.BusinessEntityID");
+
+                entity.Property(e => e.Revision)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsFixedLength(true)
+                    .HasComment("Revision number of the document. ");
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())")
+                    .HasComment("ROWGUIDCOL number uniquely identifying the record. Required for FileStream.");
+
+                entity.Property(e => e.Status).HasComment("1 = Pending approval, 2 = Approved, 3 = Obsolete");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasComment("Title of the document.");
             });
 
             OnModelCreatingPartial(modelBuilder);
---
+++ ProductApi/ProductApi/Models/Document.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ProductApi.Models
+{
+    public partial class Document
+    {
+        public HierarchyId DocumentNode { get; set; }
+        public short? DocumentLevel { get; set; }
+        public string Title { get; set; }
+        public int Owner { get; set; }
+        public bool FolderFlag { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string Revision { get; set; }
+        public int ChangeNumber { get; set; }
+        public byte Status { get; set; }
+        public string DocumentSummary { get; set; }
+        public byte[] Document1 { get; set; }
+        public Guid Rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}

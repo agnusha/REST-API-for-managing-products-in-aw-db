1249af33844db580bc3d3f0881c32221bfbc39ea
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-12-16 12:43:55 +0300

    fix: no errors

---
+++ ProductApi/FunctionSaveToDocuments/Models/AdventureWorks2019Context.cs
@@ -0,0 +1,171 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace FunctionSaveToDocuments.Models
+{
+    [ExcludeFromCodeCoverage]
+    public partial class AdventureWorks2019Context : DbContext
+    {
+        public AdventureWorks2019Context()
+        {
+        }
+
+        public AdventureWorks2019Context(DbContextOptions<AdventureWorks2019Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Document> Documents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Document>(entity =>
+            {
+                entity.HasKey(e => e.DocumentNode)
+                    .HasName("PK_Document_DocumentNode");
+
+                entity.ToTable("Document", "Production");
+
+                entity.HasComment("Product maintenance documents.");
+
+                entity.HasIndex(e => new { e.DocumentLevel, e.DocumentNode }, "AK_Document_DocumentLevel_DocumentNode")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Rowguid, "AK_Document_rowguid")
+                    .IsUnique();
+
+                entity.HasIndex(e => new { e.FileName, e.Revision }, "IX_Document_FileName_Revision");
+
+                entity.HasIndex(e => e.Rowguid, "UQ__Document__F73921F7A5124F78")
+                    .IsUnique();
+
+                entity.Property(e => e.DocumentNode).HasComment("Primary key for Document records.");
+
+                entity.Property(e => e.ChangeNumber).HasComment("Engineering change approval number.");
+
+                entity.Property(e => e.Document1)
+                    .HasColumnName("Document")
+                    .HasComment("Complete document.");
+
+                entity.Property(e => e.DocumentLevel)
+                    .HasComputedColumnSql("([DocumentNode].[GetLevel]())", false)
+                    .HasComment("Depth in the document hierarchy.");
+
+                entity.Property(e => e.DocumentSummary).HasComment("Document abstract.");
+
+                entity.Property(e => e.FileExtension)
+                    .IsRequired()
+                    .HasMaxLength(8)
+                    .HasComment("File extension indicating the document type. For example, .doc or .txt.");
+
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasMaxLength(400)
+                    .HasComment("File name of the document");
+
+                entity.Property(e => e.FolderFlag).HasComment("0 = This is a folder, 1 = This is a document.");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("Date and time the record was last updated.");
+
+                entity.Property(e => e.Owner).HasComment("Employee who controls the document.  Foreign key to Employee.BusinessEntityID");
+
+                entity.Property(e => e.Revision)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsFixedLength(true)
+                    .HasComment("Revision number of the document. ");
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())")
+                    .HasComment("ROWGUIDCOL number uniquely identifying the record. Required for FileStream.");
+
+                entity.Property(e => e.Status).HasComment("1 = Pending approval, 2 = Approved, 3 = Obsolete");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasComment("Title of the document.");
+            });
+
+            modelBuilder.Entity<Document>(entity =>
+            {
+                entity.ToTable("Document", "Production");
+
+                entity.HasKey(e => e.DocumentNode)
+                    .HasName("PK_Document_DocumentNode");
+
+                entity.HasComment("Product maintenance documents.");
+
+                entity.HasIndex(e => new { e.DocumentLevel, e.DocumentNode }, "AK_Document_DocumentLevel_DocumentNode")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Rowguid, "AK_Document_rowguid")
+                    .IsUnique();
+
+                entity.HasIndex(e => new { e.FileName, e.Revision }, "IX_Document_FileName_Revision");
+
+                entity.HasIndex(e => e.Rowguid, "UQ__Document__F73921F7A5124F78")
+                    .IsUnique();
+
+                entity.Property(e => e.DocumentNode).HasComment("Primary key for Document records.");
+
+                entity.Property(e => e.ChangeNumber).HasComment("Engineering change approval number.");
+
+                entity.Property(e => e.Document1)
+                    .HasColumnName("Document")
+                    .HasComment("Complete document.");
+
+                entity.Property(e => e.DocumentLevel)
+                    .HasComputedColumnSql("([DocumentNode].[GetLevel]())", false)
+                    .HasComment("Depth in the document hierarchy.");
+
+                entity.Property(e => e.DocumentSummary).HasComment("Document abstract.");
+
+                entity.Property(e => e.FileExtension)
+                    .IsRequired()
+                    .HasMaxLength(8)
+                    .HasComment("File extension indicating the document type. For example, .doc or .txt.");
+
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasMaxLength(400)
+                    .HasComment("File name of the document");
+
+                entity.Property(e => e.FolderFlag).HasComment("0 = This is a folder, 1 = This is a document.");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())")
+                    .HasComment("Date and time the record was last updated.");
+
+                entity.Property(e => e.Owner).HasComment("Employee who controls the document.  Foreign key to Employee.BusinessEntityID");
+
+                entity.Property(e => e.Revision)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsFixedLength(true)
+                    .HasComment("Revision number of the document. ");
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())")
+                    .HasComment("ROWGUIDCOL number uniquely identifying the record. Required for FileStream.");
+
+                entity.Property(e => e.Status).HasComment("1 = Pending approval, 2 = Approved, 3 = Obsolete");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasComment("Title of the document.");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- ProductApi/FunctionSaveToDocuments/SavingToDocument.cs
+++ ProductApi/FunctionSaveToDocuments/SavingToDocument.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using System.Threading.Tasks;
@@ -32,8 +31,34 @@ namespace FunctionSaveToDocuments
         public static async Task RunAsync([QueueTrigger("awfilequeue", Connection = "Queuetriggerconnectionstring")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-            var bytes = await DownloadFileFromBlobAsync("photo_2020-10-06_21-13-41.jpg");
-            Console.WriteLine(bytes.Length);
+            //var bytes = await DownloadFileFromBlobAsync("photo_2020-10-06_21-13-41.jpg");
+            var connectionString = "DefaultEndpointsProtocol = https; AccountName = awfile; AccountKey = HDeYG5nDvSsUABpHgp2UJXOcf7z8PQxibgdU4DgchYc2R2MXtXKfZ3aXRaZ0IVSlCVpBy4hoCif / TRKsHnNBgA ==; EndpointSuffix = core.windows.net";
+            //var storageAccount = CloudStorageAccount.Parse(connectionString);
+            //var blobClient = storageAccount.CreateCloudBlobClient();
+            //var container = blobClient.GetContainerReference("awfilecontainer");
+            //var blockBlob = container.GetBlockBlobReference("photo_2020-10-06_21-13-41.jpg");
+            //using var ms = new MemoryStream();
+            //if (await blockBlob.ExistsAsync())
+            //{
+            //    await blockBlob.DownloadToStreamAsync(ms);
+            //}
+
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            // Get Blob Container  
+            var container = blobClient.GetContainerReference("awfilecontainer");
+            // Get reference to blob (binary content)  
+            var blockBlob = container.GetBlockBlobReference("photo_2020-10-06_21-13-41.jpg");
+            // Read content  
+            using (var ms = new MemoryStream())
+            {
+                if (await blockBlob.ExistsAsync())
+                {
+                    await blockBlob.DownloadToStreamAsync(ms);
+                }
+                var bytes = ms.ToArray();
+                Console.WriteLine(bytes.Length);
+            }
         }
     }
 }

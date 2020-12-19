3c0e8ade6ae14527d985eda94b2750bd582748b3
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2020-12-19 14:58:18 +0300

    feat: try use ef in old func

--- ProductApi/FunctionSaveToDocuments/SavingToDocument.cs
+++ ProductApi/FunctionSaveToDocuments/SavingToDocument.cs
@@ -6,6 +6,7 @@ using Microsoft.WindowsAzure.Storage;
 using System.Threading.Tasks;
 using System.Configuration;
 using FunctionSaveToDocuments.Models;
+using System.Data.SqlClient;
 
 namespace FunctionSaveToDocuments
 {
@@ -16,11 +17,6 @@ namespace FunctionSaveToDocuments
     
     public class SavingToDocument
     {
-        private readonly AdventureWorksFuncContext _context;
-        public SavingToDocument(AdventureWorksFuncContext context)
-        {
-            _context = context;
-        }
 
         [FunctionName("FunctionSaveToDocument")]
         public static async Task RunAsync([QueueTrigger("awfilequeue", Connection = "Queuetriggerconnectionstring")]string myQueueItem, ILogger log)
@@ -56,11 +52,36 @@ namespace FunctionSaveToDocuments
             }
         }
 
-        private async Task<Document> PostDocument(Document document)
+        private async Task<int> InsertDocument(Document document)
         {
-            await _context.Documents.AddAsync(document);
-            await _context.SaveChangesAsync();
-            return document;
+                const string connectionString =
+                    @"Server=tcp:aw-vm-sql-server.database.windows.net,1433;Initial Catalog=aw-vm-sql_2020-12-12T18-24Z;Persist Security Info=False;User ID=agnia;Password=Epam2020Create;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                const string sqlExpression =
+                    "INSERT INTO Production.Document (DocumentNode, Title, Owner, FolderFlag, FileName, FileExtension, Revision, ChangeNumber, Status, DocumentSummary) VALUES " +
+                    "('/'+CAST((select count(DocumentNode) from Production.Document) AS VARCHAR(10))+'/', " +
+                    "@Title, @Owner, @FolderFlag, @FileName, @FileExtension, @Revision, @ChangeNumber, @Status, @DocumentSummary)";
+
+                await using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                var command = new SqlCommand(sqlExpression, connection);
+
+                command.Parameters.AddWithValue("@Title", "hey 2");
+                command.Parameters.AddWithValue("@Owner", 220);
+                command.Parameters.AddWithValue("@FolderFlag", 1);
+                command.Parameters.AddWithValue("@FileName", "test code");
+                command.Parameters.AddWithValue("@FileExtension", ".txt");
+                command.Parameters.AddWithValue("@Revision", 0);
+                command.Parameters.AddWithValue("@ChangeNumber", 0);
+                command.Parameters.AddWithValue("@Status", 1);
+                command.Parameters.AddWithValue("@DocumentSummary", "DocumentSummary");
+                //command.Parameters.AddWithValue("@Document", value2);
+
+                var number = command.ExecuteNonQuery();
+                Console.WriteLine("Добавлено документов: {0}", number);
+
+            return number;
+
         }
 
         private static async Task<byte[]> DownloadFileFromBlobAsync(string blobReferenceKey)
